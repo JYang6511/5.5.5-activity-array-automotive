8550360a24f3b8d0e473fcb70e4b443af754c750
Johnson Yang <johnsonyang65@gmail.com>
2023-08-25 17:41:29 -0700

    finished

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,14 +13,45 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule {
+    constructor (make, model, year, color, mileage){
+        super (make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4; 
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false; 
 
+    }
+    loadPassenger(num){
+        if (num <= this.maxPassengers ) {
+         this.passenger = num;
+         console.log("this car has enough space for all passengers")
+            return this.passenger;
+        } else {
+            console.log ("this car has not enough space to fit all passengers");
+        }
+     
+    }
 
+    Service(mileage) {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
 
-
-
-
-
-
+    start() {
+        if (this.fuel > 0) {
+        console.log("car starting..");
+         return this.started = true;
+        } else {
+            console.log("fuel empty");
+            return this.started = false;
+        }
+    }
+}
 
 
 
@@ -29,7 +60,15 @@ console.log(v.make)
 //You can use the same instance "v" of the Vehicle class above for the base class.
 
 
+let audiCar = new Car (Audi, RS7, 2023, black, 1000);
 
 
+audiCar.Service()
+audiCar.start()
+audiCar.loadPassenger()
 
+let bmwCar = new Car (BMW, M3, 2021, blue, 31000);
+bmwCar.Service()
+bmwCar.start()
+bmwCar.loadPassenger(6)
 //Create at least two new instances of the Car class and test them here:
